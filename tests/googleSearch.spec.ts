99c85edd827c0be695b3025cd556bceacfe598d0
Amrita Kumari <amritasumit2010@gmail.com>
2026-03-30 13:48:06 +0530

    adding allure report details

--- tests/googleSearch.spec.ts
+++ tests/googleSearch.spec.ts
@@ -22,6 +22,34 @@ test('Print Test3 description', async () => {
 });
 
 test('Print Test4 description', async () => {
+
+  function findSmallestMissingPositive() {
+    const orderNumbers = [-1,1,3,2]
+    let n = orderNumbers.length;
+
+    for (let i = 0; i < n; i++) {
+        while (
+            orderNumbers[i] > 0 &&
+            orderNumbers[i] <= n &&
+            orderNumbers[orderNumbers[i] - 1] !== orderNumbers[i]
+        ) {
+            let targetIndex = orderNumbers[i] - 1;
+
+            // swap
+            let temp = orderNumbers[i];
+            orderNumbers[i] = orderNumbers[targetIndex];
+            orderNumbers[targetIndex] = temp;
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (orderNumbers[i] !== i + 1) {
+            return i + 1;
+        }
+    }
+
+    return n + 1;
+}
   console.log('Print Test4 description:');
 });
 
